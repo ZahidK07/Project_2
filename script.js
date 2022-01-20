416b73eb52efc9478fdb41305e2cc7b5a1abd15a
ZAHID <zaksfit07@gmail.com>
2022-01-20 19:57:57 +0530

    updated

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function myOffer(){
+    document.getElementById("Coupon").style.visibility="visible"
+}
+
+function noCoupon(){
+    document.getElementById("Coupon").style.visibility="hidden"
+}
