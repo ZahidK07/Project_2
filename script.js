7dfee66d6e20c441aca01a7f23ecb48799173ee7
ZAHID <zaksfit07@gmail.com>
2022-01-31 21:11:11 +0530

    Footer and Bottom Info Added

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 function myOffer(){
     document.getElementById("Coupon").style.visibility="visible"
-}
+};
 
 function noCoupon(){
     document.getElementById("Coupon").style.visibility="hidden"
-}
+};
+
+
+        
